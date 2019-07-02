76569339f602d665a923c49512a99e14b183b699
tianyu <0318@qq.com>
2019-07-02 17:02:55 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     filename: 'index.js',// 输出文件名
   },
 
-  //所有的loader都要配置在module里
+  //所有的loader都要配置在module里,所有的loader在使用的时候，都不用引入。
   module: {
     //rules中指明loader“干活”的顺序，以及处理哪些文件。
     rules: [
@@ -25,6 +25,37 @@ module.exports = {
         }, {
           loader: "less-loader" // 将less转换成css
         }]
+      },
+
+      //使用file-loader处理图片资源
+      /*{
+        test: /\.(png|jpg|gif)$/,
+        use:[
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath:'img', //图片最终输出的位置,以输出文件夹为基准(build)
+              publicPath:'../build/img',//css资源图片路径,以src文件夹为基准
+              name:'[hash:5].[ext]'//修改图片名称
+            }
+          }
+        ]
+      },*/
+
+      //使用url-loader处理图片资源（可以转换base64编码）
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',  //如果不做图片转base64，可以用file-loader
+            options: {
+              limit: 8192,  //图片大小的敏感点，大于8KB不转换，小于8KB转成base64
+              outputPath:'img',   //图片最终输出的位置
+              publicPath:'../build/img', //css资源图片路径
+              name:'[hash:5].[ext]'  //修改图片名称
+            }
+          }
+        ]
       }
     ]
   }
